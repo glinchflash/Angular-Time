9efb5e24d5b5b917158ca9505d6d0a63628f6ffa
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-08-04 14:26:31 +0200

    added null typing to properties to set them all to null according to assignment

--- Friendbook/src/app/friend.ts
+++ Friendbook/src/app/friend.ts
@@ -1,16 +1,57 @@
 export class Friend {
-   _firstName:string;
-   _lastName:string;
-   _email:string;
-   _phoneNumber:number;
-   _favouriteLanguage:string;
+   _firstName: string | null;
+   _lastName: string | null;
+   _email: string | null;
+   _phoneNumber: number | null;
+   _favouriteLanguage: string | null;
 
 
-  constructor(firstname:string, lastname:string, email:string, phonenumber:number, favlanguage:string) {
+  constructor(firstname: string | null, lastname: string | null, email: string | null, phonenumber: number | null, favlanguage: string | null) {
     this._firstName = firstname;
     this._lastName = lastname
     this._email = email;
     this._phoneNumber = phonenumber;
     this._favouriteLanguage = favlanguage
   }
+
+  // get firstName(): string | null {
+  //   return this._firstName;
+  // }
+  //
+  // set firstName(value: string | null) {
+  //   this._firstName = value;
+  // }
+  //
+  // get lastName(): string | null {
+  //   return this._lastName;
+  // }
+  //
+  // set lastName(value: string | null) {
+  //   this._lastName = value;
+  // }
+  //
+  // get email(): string | null {
+  //   return this._email;
+  // }
+  //
+  // set email(value: string | null) {
+  //   this._email = value;
+  // }
+  //
+  // get phoneNumber(): number | null {
+  //   return this._phoneNumber;
+  // }
+  //
+  // set phoneNumber(value: number | null) {
+  //   this._phoneNumber = value;
+  // }
+  //
+  // get favouriteLanguage(): string | null {
+  //   return this._favouriteLanguage;
+  // }
+  //
+  // set favouriteLanguage(value: string | null) {
+  //   this._favouriteLanguage = value;
+  // }
+
 }
