6d19b08da76a52cacff4e7e236c9f57d6291e262
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-08-04 09:29:53 +0200

    created submitform method, added language array and friendmodel property

--- Friendbook/src/app/app.component.ts
+++ Friendbook/src/app/app.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import {Friend} from "./friend";
+import {AddFriendService} from "./add-friend.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  private addFriendService: AddFriendService;
+   languageArray:Array<string> = ['HTML', 'css','javascript','php', 'angular', 'python']
+    friendModel = new Friend('','','',0,'')
+
+  constructor() {
+    
+  }
+
+  submitForm(){
+     addFriend(this.friendModel)
+    console.log(this.friendModel);
+  }
+}
