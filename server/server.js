dd10de72f06724a048e6dabdd2e3a0ee1fed36e0
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-08-05 12:23:06 +0200

    added get and post functions for seeing all friends and adding friends

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {response} = require("express");
 const app = express();
 
 const PORT = 9099;
@@ -8,7 +7,7 @@ const PORT = 9099;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}) );
 
-app.all("/*", function(req, res, next){
+app.all("/*", function(_req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
@@ -20,19 +19,22 @@ let allFriends = [{fName: 'Coach', lName: 'Tim', email: 'tim.broos@becode.org', 
 
 // Below you can define how your API handles a get or a post request.
 // Try sending a get request to the root, you should get a "Hello from server" back.
-app.get('/allFriends',function (request,response) {
+
+app.get('/allFriends', function (_request,response){
     response.send(allFriends);
 })
 
-app.get('/', function (request, response) {
+app.get('/', function (_request, response) {
     response.send('Hello from server');
 });
 
-app.post('/', function (request, response) {
+app.post('/', function (_request, response) {
     response.status(200).send({"message": "Data received"});
 });
 
-app.post('/allFriends',function (request, response) {
-    response.status(200).send(allFriends);
-})
-app.listen(PORT, function () {});
+app.post('/addFriend',function (_request) {
+    allFriends.push(_request.body)
+});
+
+
+app.listen(PORT, function () {console.log('server active')});
